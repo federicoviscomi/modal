b9d9b89ae9d984a82c0e360a7b3c9985f8829f45
fevi_1spatial <federico.viscomi@1spatial.com>
2023-03-10 09:49:57 +0000

    end of section 15 - but really - previous commit message was meant to say 14

---
+++ src/pages/TablePage.js
@@ -0,0 +1,34 @@
+import Table from '../components/Table';
+
+function TablePage() {
+    const data = [
+        {name: 'orange', color: 'bg-orange-500', score: 5},
+        {name: 'apple', color: 'bg-red-500', score: 3},
+        {name: 'banana', color: 'bg-yellow-500', score: 1},
+        {name: 'lime', color: 'bg-green-500', score: 4},
+    ];
+    const config = [
+        {
+            label: 'Name',
+            render: (fruit) => fruit.name
+        },
+        {
+            label: 'Color',
+            render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}/>
+        },
+        {
+            label: 'Score',
+            render: (fruit) => fruit.score
+        },
+    ];
+    const keyFn = (fruit) => {
+        return fruit.name;
+    }
+    return (
+        <div>
+            <Table data={data} config={config} keyFn={keyFn}></Table>
+        </div>
+    );
+}
+
+export default TablePage;
